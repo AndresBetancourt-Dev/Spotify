1957cc0eb6fc9bfd2e22f94a0237879ae7b950be
Andrés David Betancourt Santana <65095693+AndresBetancourt-Dev@users.noreply.github.com>
2022-01-14 02:44:51 -0500

    🎡 Added Recoil, Modified Poster and Created Track Component

--- components/Body.tsx
+++ components/Body.tsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react"
 import { useSession } from 'next-auth/react';
 import { Search } from "./Search"
 import { Poster } from "./Poster";
+import { Track } from "./Track";
 
 
-export const Body = ({ spotifyApi }) => {
+export const Body = ({ spotifyApi, chooseTrack }) => {
 
     const { data: session } = useSession();
     const { accessToken } = session;
     const [search, setSearch] = useState<string>("");
     const [searchResults, setSearchResults] = useState<Array<Object>>([]);
     const [newReleases, setNewReleases] = useState<Object[]>([]);
+
+
 
 
     useEffect(() => {
@@ -58,9 +61,6 @@ export const Body = ({ spotifyApi }) => {
         });
     }, [accessToken]);
 
-    const chooseTrack = () => {
-
-    }
 
     return (
         <section className="body md:max-w-6x1">
@@ -70,6 +70,51 @@ export const Body = ({ spotifyApi }) => {
                     <Poster key={track.id} track={track} chooseTrack={chooseTrack} />
                 )) : searchResults.slice(0, 4).map(track => (<Poster key={track.id} track={track} chooseTrack={chooseTrack} />))}
             </div>
-        </section>
+            <div className="flex gap-x-8 absolute min-w-full md:relative ml-6">
+                {/* Genres */}
+                <div className="hidden xl:inline max-w-[270px]">
+                    <h2 className="text-white font-bold mb-3">Genres</h2>
+                    <div className="flex gap-x-2 gap-y-2.5 flex-wrap mb-3">
+                        <div className="genre">Classic</div>
+                        <div className="genre">House</div>
+                        <div className="genre">Minimal</div>
+                        <div className="genre">Hip-hop</div>
+                        <div className="genre">Electronic</div>
+                        <div className="genre">Chillout</div>
+                        <div className="genre">Blues</div>
+                        <div className="genre">Country</div>
+                        <div className="genre">Techno</div>
+                    </div>
+                    <button className="button">
+                        All Genres
+                    </button>
+                </div>
+                {/* Tracks */}
+                <div>
+                    <h2 className="text-white font-bold mb-3">{searchResults.length === 0 ? "New Releases" : "Tracks"}</h2>
+                    <div className="space-y-3 border-2 border-[#262626] rounded-2xl p-3 bg-[#0D0D0D] overflow-y-scroll h-[1000px] md:h-96 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-thumb-rounded hover:scrollbar-thumb-gray-500 w-[830px]">
+                        {searchResults.length === 0
+                            ? newReleases
+                                .slice(4, newReleases.length)
+                                .map((track) => (
+                                    <Track
+                                        key={track.id}
+                                        track={track}
+                                        chooseTrack={chooseTrack}
+                                    />
+                                ))
+                            : searchResults
+                                .slice(4, searchResults.length)
+                                .map((track) => (
+                                    <Track
+                                        key={track.id}
+                                        track={track}
+                                        chooseTrack={chooseTrack}
+                                    />
+                                ))}
+                    </div>
+                </div>
+            </div>
+        </section >
     )
 }
--- components/Poster.tsx
+++ components/Poster.tsx
@@ -1,13 +1,29 @@
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs"
+import { useRecoilState } from "recoil"
+import { playingTrackState, playState } from "../atoms/playerAtom"
 
-export const Poster = (props) => {
+export const Poster = ({ track, chooseTrack }) => {
+    const [play, setPlay] = useRecoilState(playState);
+    const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+
+    const handlePlay = () => {
+        chooseTrack(track);
+        if (track.uri === playingTrack.uri) {
+            setPlay(!play)
+        }
+    }
+
     return (
-        <div className="poster group">
-            <img className="h-full w-full object-cover absolute inset-0 rounded-[50px] opacity-80 group-hover:opacity-100" src={props.track.albumUrl} alt={props.track.title} />
+        <div className="poster group" onClick={handlePlay}>
+            <img className="h-full w-full object-cover absolute inset-0 rounded-[50px] opacity-80 group-hover:opacity-100" src={track.albumUrl} alt={track.title} />
             <div className="absolute bottom-10 inset-x-0 ml-4 flex items-center space-x-3.5">
                 <div className="h-10 w-10 bg-[#15883e] rounded-full flex items-center justify-center flex-shrink-0 group-hover:bg-[#1db954] ">
-                    <BsFillPauseFill className="text-xl" />
-                    <BsFillPlayFill className="text-xl ml-[1px]" />
+                    {track.uri === playingTrack.uri && play ? <BsFillPauseFill className="text-xl" /> : <BsFillPlayFill className="text-xl ml-[1px]" />}
+                </div>
+                <div className="text-[15px]">
+                    <h4 className="font-extrabold truncate w-44">{track.title}</h4>
+                    <h6>{track.artist}</h6>
                 </div>
             </div>
         </div>
